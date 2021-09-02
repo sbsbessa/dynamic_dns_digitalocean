49f1fdd8cf95602ec265788e56f7b489a1943c4d
John Maximilian <2e0byo@gmail.com>
2021-09-02 17:57:43 +0100

    make sh compatible and handle multiple ids

--- update_dns.sh
+++ update_dns.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+
+id=$(echo ${3} | awk '{ print $1 }')
+
 
 CURRENT_IP=$(dig @resolver1.opendns.com A myip.opendns.com +short -4)
 echo "Current IP: ${CURRENT_IP}"
 
-echo curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${1}" "https://api.digitalocean.com/v2/domains/${2}/records/${3}"
-DOMAIN_DATA=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${1}" "https://api.digitalocean.com/v2/domains/${2}/records/${3}")
+echo curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${id}" "https://api.digitalocean.com/v2/domains/${2}/records/${3}"
+DOMAIN_DATA=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${1}" "https://api.digitalocean.com/v2/domains/${2}/records/${id}")
 
-REGISTERED_IP=$( jq -r '.domain_record.data' <<< "${DOMAIN_DATA}" )
+
+REGISTERED_IP=$( echo ${DOMAIN_DATA} | jq -r '.domain_record.data')
 echo "Registered IP: ${REGISTERED_IP}"
 
 # check if IP Address has changed
 if [ "$CURRENT_IP" != "$REGISTERED_IP" ]
 then
   echo "IP has changed"
-  UPDATE_RESULT=$(curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $1" -d "{\"data\":\"${CURRENT_IP}\"}" "https://api.digitalocean.com/v2/domains/$2/records/$3")
+  for id in $(echo ${3} | sed 's/ /\n/g')
+    do
+      UPDATE_RESULT=$(curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $1" -d "{\"data\":\"${CURRENT_IP}\"}" "https://api.digitalocean.com/v2/domains/$2/records/$id")
+    done
   echo "DNS updated to $CURRENT_IP"
 else
   echo "IP did not change"
